58a35d2b3148ea5522aa74cb1f39b58c4bd66166
salihcelebi96 <celebisalih277@gmail.com>
2023-10-31 19:53:38 +0300

    14.commit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: React.FC = () => {
              
         </div>
       <nav className='w-screen'>
-        <ul className='flex justify-center gap-10  items-center  h-16 bg-blue-700 text-white lg:text-2xl  sm:text-xl'>
+        <ul className='flex justify-center gap-10 mx-2  items-center  h-16 bg-blue-700 text-white lg:text-2xl  sm:text-xl'>
           <li>
             <Link className="navbar-link" to="/">Anasayfa</Link>
           </li>
